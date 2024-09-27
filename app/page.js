438d94e17f86f5aa2a58c3f315a8164d93686eac
sibiraj-002 <shansibi2002@gmail.com>
2024-09-27 14:37:56 +0530

    Home page slug removed

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import HomeComponent from "../Components/HomeComponents"
 export default function Home() {
   return (
     <>
-    
+    <HomeComponent />
     </>
   );
 }
